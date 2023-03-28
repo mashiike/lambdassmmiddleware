b50022968d21038daa29b8c70d15fe0872418b17
mashiike <ikeda-masashi@kayac.com>
2023-03-28 16:51:28 +0900

    add lambda sample

---
+++ _example/example.tf
@@ -0,0 +1,63 @@
+
+resource "aws_iam_role" "lambdassmmiddleware" {
+  name = "lambdassmmiddleware"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambdassmmiddleware" {
+  name   = "lambdassmmiddleware"
+  path   = "/"
+  policy = data.aws_iam_policy_document.lambdassmmiddleware.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambdassmmiddleware" {
+  role       = aws_iam_role.lambdassmmiddleware.name
+  policy_arn = aws_iam_policy.lambdassmmiddleware.arn
+}
+
+data "aws_iam_policy_document" "lambdassmmiddleware" {
+  statement {
+    actions = [
+      "ssm:GetParameter*",
+      "ssm:DescribeParameters",
+      "ssm:List*",
+    ]
+    resources = ["*"]
+  }
+  statement {
+    actions = [
+      "logs:GetLog*",
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_ssm_parameter" "foo" {
+  name        = "/lambdassmmiddleware/foo"
+  description = "foo for lambdassmmiddleware"
+  type        = "String"
+  value       = "foo values"
+}
+
+resource "aws_ssm_parameter" "bar" {
+  name        = "/lambdassmmiddleware/bar"
+  description = "bar for lambdassmmiddleware"
+  type        = "String"
+  value       = "bar values"
+}
--- _example/main.go
+++ _example/main.go
@@ -1,8 +1,3 @@
-# lambdassmmiddlware
-AWS Lambda middleware of ssm parameters middleware for Golang
-
-
-```go
 package main
 
 import (
@@ -43,21 +38,3 @@ func main() {
 	}
 	lambda.Start(handler)
 }
-```
-
-see deteils [_example/](_example/)
-
-```shell
-$ make lambroll/invoke 
-lambroll invoke
-2023/03/28 16:50:09 [info] lambroll v0.14.2 with function.json
-Enter JSON payloads for the invoking function into STDIN. (Type Ctrl-D to close.)
-{}
-{"bar":"bar values","env_bar":"bar values","env_foo":"foo values","foo":"foo values","tora":null}
-2023/03/28 16:50:20 [info] StatusCode:200
-2023/03/28 16:50:20 [info] ExecutionVersion:$LATEST
-```
-
-## LICENSE 
-
-MIT
